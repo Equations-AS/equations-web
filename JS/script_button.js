75185b03aa8215ed9144cc0840536f531ead2056
AYUREVA <yap0515@gmail.com>
2025-01-23 14:33:19 +1000

    испарвил рандом

--- JS/script_button.js
+++ JS/script_button.js
@@ -10,24 +10,24 @@ const random = document.getElementById('random');
 btn.onclick = function() {
     var rand = Random(0, 6)
     if (rand == 0) {
-        random.innerHTML = '<a href="pages/line.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/line.html">Нажми</a>';
     }
     else if (rand == 1) {
-        random.innerHTML = '<a href="pages/kvad.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/kvad.html">Нажми</a>';
     }
     else if (rand == 2) {
-        random.innerHTML = '<a href="pages/log.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/log.html">Нажми</a>';
     }
     else if (rand == 3) {
-        random.innerHTML = '<a href="pages/module.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/module.html">Нажми</a>';
     }
     else if (rand == 4) {
-        random.innerHTML = '<a href="pages/kor.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/kor.html">Нажми</a>';
     }
     else if (rand == 5) {
-        random.innerHTML = '<a href="pages/step.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/step.html">Нажми</a>';
     }
     else if (rand == 6) {
-        random.innerHTML = '<a href="pages/drob.html">Нажми</a>';
+        random.innerHTML = '<a href="../pages/drob.html">Нажми</a>';
     }
 }
